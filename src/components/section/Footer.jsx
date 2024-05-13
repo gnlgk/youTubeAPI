205895d0e61ad14927adbd66c4cf92b5eb3b1d06
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-05-13 14:08:33 +0900

    202

---
+++ src/components/section/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer id='footer' role='contentinfo'>
+        footer
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
